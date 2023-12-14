553c662e1eae7f79c5139606c2ac549f1b9ded1a
iagomoraisn <iagojkd96@hotmail.com>
2023-12-14 13:19:08 -0300

    oneKeyDown

--- githubFinder/src/components/Search.tsx
+++ githubFinder/src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>;
 };
 
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 import { BsSearch } from "react-icons/bs";
 
@@ -10,13 +10,19 @@ import classes from "./Search.module.css";
 
 const Search = ({loadUser}: SearchProps) => {
     const [userName, setUserName] = useState("");
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if(e.key === "Enter") {
+}
+    }
     return (
         <div className={classes.search}>
             <h2>Busque por um usúario:</h2>
             <p>Conheça seus melhores repositórios</p>
         <div className={classes.search_container}>
         <input type="text" placeholder="Digite o nome do usuário"
-        onChange={(e) => setUserName(e.target.value)} />
+        onChange={(e) => setUserName(e.target.value)}
+        oneKeyDown={handleKeyDown} />
         <button onClick={() => loadUser(userName)}>
             <BsSearch />
         </button>
